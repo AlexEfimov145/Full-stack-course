4bb6830c0c344c4f43c3d39ef0994643537bd133
AlexEfimov145 <AlexEfimov145@gmail.co.il>
2023-08-19 21:18:59 +0300

    creation

--- new folder/homework/homework 17.8/assets/js/js for url input.js
+++ new folder/homework/homework 17.8/assets/js/js for url input.js
@@ -1,10 +1,13 @@
-function loadImage(){
+console.log("lllll");
+function loadImage() {
+  try {
     var img = document.getElementById("imageURL").value;
     var friendName = document.getElementById("friendName").value;
     var friendTel = document.getElementById("friendTel").value;
-    document.getElementById("friendList").innerHTML=`
+
+    document.getElementById("friendList").innerHTML += `
     <tr>
-      <td>
+      <td class = "ggg">
          <img src="${img}" width="50"/>
       </td>
       <td>
@@ -13,4 +16,25 @@ function loadImage(){
       <td>
         ${friendTel}
       </td>`;
+  } catch (error) {
+    throw new Error("Couldn't load image");
+  }
 }
+//     var img = document.getElementById("imageURL").value;
+//     var friendName = document.getElementById("friendName").value;
+//     var friendTel = document.getElementById("friendTel").value;
+
+//     document.getElementById("friendList").innerHTML+=`
+//     <tr>
+//       <td class = "ggg">
+//          <img src="${img}" width="50"/>
+//       </td>
+//       <td>
+//         ${friendName}
+//       </td>
+//       <td>
+//         ${friendTel}
+//       </td>`;
+//       console.log(Math.random());
+// }
+// var click = document.getElementById("click");

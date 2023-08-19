e85e2cef196a6766f5d14a376319d5462b57e5a1
AlexEfimov145 <AlexEfimov145@gmail.co.il>
2023-08-19 18:45:01 +0300

    homework

---
+++ new folder/homework/homework 17.8/assets/js/js for url input.js
@@ -0,0 +1,16 @@
+function loadImage(){
+    var img = document.getElementById("imageURL").value;
+    var friendName = document.getElementById("friendName").value;
+    var friendTel = document.getElementById("friendTel").value;
+    document.getElementById("friendList").innerHTML=`
+    <tr>
+      <td>
+         <img src="${img}" width="50"/>
+      </td>
+      <td>
+        ${friendName}
+      </td>
+      <td>
+        ${friendTel}
+      </td>`;
+}

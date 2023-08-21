6fff47c5809dd9f33a1ff5fc241f9935db020c4a
AlexEfimov145 <AlexEfimov145@gmail.co.il>
2023-08-21 18:32:03 +0300

    array fix

--- new folder/homework/homework 17.8/assets/js/js_for_url_input.js
+++ new folder/homework/homework 17.8/assets/js/js_for_url_input.js
@@ -1,17 +1,37 @@
-function loadImage() {
-  var img = document.getElementById("imageURL").value;
-  var friendName = document.getElementById("friendName").value;
-  var friendTel = document.getElementById("friendTel").value;
+var imageURL = [];
+var friendTel = [];
+var friendName = [];
 
-  document.getElementById("friendList").innerHTML += `
-     <tr>
-       <td class = "ggg">
-          <img src="${img}" width="100"/>
-       </td>
-       <td>
-         ${friendName}
-       </td>
-       <td>
-         ${friendTel}
-       </td>`;
+function loadImage() {
+  var urlData = document.getElementById("imageURL").value;
+  var telData = document.getElementById("friendName").value;
+  var nameData = document.getElementById("friendTel").value;
+
+  imageURL.push(urlData);
+  friendTel.push(telData);
+  friendName.push(nameData);
+
+  var data = document.getElementById("data");
+
+  var info = "";
+
+  for (var index = 0; index < imageURL.length; index++) {
+    info += `
+  <tr>
+    <td>
+       <img src="${imageURL[index]}"/>
+    </td>
+    <td>
+      ${friendName[index]}
+    </td>
+    <td>
+      ${friendTel[index]}
+    </td>
+    </tr>`;
+  }
+
+  data.innerHTML = info;
+  urlData = "";
+  telData = "";
+  nameData = "";
 }
